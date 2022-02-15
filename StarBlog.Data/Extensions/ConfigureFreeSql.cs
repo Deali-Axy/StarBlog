0d4744c1c9252dda36aebe2625f701061bf554c2
DealiAxy <dealiaxy@gmail.com>
2022-02-15 21:52:08 +0800

    界面的引用库啥的基本OK

---
+++ StarBlog.Data/Extensions/ConfigureFreeSql.cs
@@ -0,0 +1,19 @@
+﻿using FreeSql;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StarBlog.Data.Extensions;
+
+public static class ConfigureFreeSql {
+    public static void AddFreeSql(this IServiceCollection services, IConfiguration configuration) {
+        var freeSql = new FreeSqlBuilder()
+            .UseConnectionString(DataType.Sqlite, configuration.GetConnectionString("SQLite"))
+            .UseAutoSyncStructure(true)
+            .Build();
+
+        services.AddSingleton(freeSql);
+
+        // 仓储模式支持
+        services.AddFreeRepository();
+    }
+}
