771f019f587a0a3d8d456fb1ab4ed06f83e3269c
DealiAxy <dealiaxy@gmail.com>
2022-02-15 23:54:21 +0800

    博客展示功能完成

--- StarBlog.Web/Program.cs
+++ StarBlog.Web/Program.cs
@@ -1,7 +1,10 @@
+using StarBlog.Data.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddFreeSql(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ StarBlog.Web/ViewModels/BlogList.cs
@@ -0,0 +1,7 @@
+﻿using StarBlog.Data.Models;
+
+namespace StarBlog.Web.ViewModels; 
+
+public class BlogList {
+    public List<Post> Posts { get; set; }
+}
