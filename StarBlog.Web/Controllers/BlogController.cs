2b46fe023758c80d165b39292e61e2007a1c5880
DealiAxy <dealiaxy@gmail.com>
2022-02-15 23:54:21 +0800

    博客展示功能完成

--- StarBlog.Web/Controllers/BlogController.cs
+++ StarBlog.Web/Controllers/BlogController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+using StarBlog.Web.ViewModels;
 
-namespace StarBlog.Web.Controllers; 
+namespace StarBlog.Web.Controllers;
 
 public class BlogController : Controller {
-    // GET
+    private readonly IBaseRepository<Post> _postRepo;
+    private readonly IBaseRepository<Category> _categoryRepo;
+
+    public BlogController(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo) {
+        _postRepo = postRepo;
+        _categoryRepo = categoryRepo;
+    }
+
     public IActionResult List() {
-        return View();
+        return View(new BlogList {
+            Posts = _postRepo.Select.ToList()
+        });
     }
 }
