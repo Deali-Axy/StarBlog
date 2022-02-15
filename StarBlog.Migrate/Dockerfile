a1baa9da6395b3f526788121c1aba07e716ca013
DealiAxy <dealiaxy@gmail.com>
2022-02-15 23:54:07 +0800

    导入功能完成

---
+++ StarBlog.Migrate/Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["StarBlog.Migrate/StarBlog.Migrate.csproj", "StarBlog.Migrate/"]
+RUN dotnet restore "StarBlog.Migrate/StarBlog.Migrate.csproj"
+COPY . .
+WORKDIR "/src/StarBlog.Migrate"
+RUN dotnet build "StarBlog.Migrate.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "StarBlog.Migrate.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "StarBlog.Migrate.dll"]
---
+++ StarBlog.Migrate/GuidUtils.cs
@@ -0,0 +1,35 @@
+﻿namespace StarBlog.Migrate;
+
+public static class GuidUtils {
+    /// <summary>
+    /// 由连字符分隔的32位数字
+    /// </summary>
+    /// <returns></returns>
+    private static string GetGuid() {
+        var guid = new Guid();
+        guid = Guid.NewGuid();
+        return guid.ToString();
+    }
+
+    /// <summary>  
+    /// 根据GUID获取16位的唯一字符串  
+    /// </summary>  
+    /// <returns></returns>  
+    public static string GuidTo16String() {
+        long i = 1;
+        foreach (var b in Guid.NewGuid().ToByteArray()) {
+            i *= b + 1;
+        }
+
+        return $"{i - DateTime.Now.Ticks:x}";
+    }
+
+    /// <summary>  
+    /// 根据GUID获取19位的唯一数字序列  
+    /// </summary>  
+    /// <returns></returns>  
+    public static long GuidToLongID() {
+        var buffer = Guid.NewGuid().ToByteArray();
+        return BitConverter.ToInt64(buffer, 0);
+    }
+}
