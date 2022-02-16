1d04a3dc2cffee41e61cd6064b7ab0800aaddca0
DealiAxy <dealiaxy@gmail.com>
2022-02-16 16:50:36 +0800

    增加了字符串操作扩展

---
+++ StarBlog.Contrib/Extensions/StringExt.cs
@@ -0,0 +1,11 @@
+﻿namespace StarBlog.Contrib.Extensions; 
+
+public static class StringExt {
+    public static string Limit(this string str,int length) {
+        if (str.Length<=length) {
+            return str;
+        }
+
+        return str[..length];
+    }
+}
