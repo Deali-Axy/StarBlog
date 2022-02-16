493e1b31cb9371e585fa62036dab3c07b699ac30
DealiAxy <dealiaxy@gmail.com>
2022-02-16 22:39:55 +0800

    博客文章增加了新建时间和修改时间字段；增加一个字段用来保存类别层级

--- StarBlog.Data/Models/Post.cs
+++ StarBlog.Data/Models/Post.cs
@@ -9,12 +9,46 @@ public class Post {
     [Column(IsIdentity = false, IsPrimary = true)]
     public string Id { get; set; }
 
+    /// <summary>
+    /// 标题
+    /// </summary>
     public string Title { get; set; }
-    
+
+    /// <summary>
+    /// 梗概
+    /// </summary>
     public string Summary { get; set; }
+
+    /// <summary>
+    /// 内容（markdown格式）
+    /// </summary>
     public string Content { get; set; }
+
+    /// <summary>
+    /// 博客相对路径
+    /// <para>如："系列/AspNetCore开发笔记"</para>
+    /// </summary>
     public string Path { get; set; }
 
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    public DateTime CreationTime { get; set; }
+
+    /// <summary>
+    /// 上次更新时间
+    /// </summary>
+    public DateTime LastUpdateTime { get; set; }
+
+    /// <summary>
+    /// 分类ID
+    /// </summary>
     public int CategoryId { get; set; }
+
+    /// <summary>
+    /// 分类
+    /// </summary>
     public Category Category { get; set; }
+
+    public string Categories { get; set; }
 }
