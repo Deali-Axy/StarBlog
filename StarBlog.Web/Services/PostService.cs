b5190f8a529d2bc0e22032dd29da5f0cdbb0c572
DealiAxy <dealiaxy@gmail.com>
2022-02-17 00:19:44 +0800

    添加了服务层

---
+++ StarBlog.Web/Services/PostService.cs
@@ -0,0 +1,54 @@
+﻿using FreeSql;
+using Markdig;
+using Markdig.Prism;
+using Markdown.ColorCode;
+using StarBlog.Data.Models;
+using StarBlog.Web.ViewModels;
+
+namespace StarBlog.Web.Services;
+
+public class PostService {
+    private readonly IBaseRepository<Post> _postRepo;
+    private readonly IBaseRepository<Category> _categoryRepo;
+
+    public PostService(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo) {
+        _postRepo = postRepo;
+        _categoryRepo = categoryRepo;
+    }
+
+    public PostViewModel GetPostViewModel(Post post) {
+        var mdPipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .UsePipeTables()
+            .UseEmphasisExtras()
+            .UseGenericAttributes()
+            .UseDefinitionLists()
+            .UseAutoIdentifiers()
+            .UseAutoLinks()
+            .UseTaskLists()
+            .UseBootstrap()
+            // .UsePrism()
+            .UseColorCode()
+            .Build();
+
+        var vm = new PostViewModel {
+            Id = post.Id,
+            Title = post.Title,
+            Summary = post.Summary,
+            Content = post.Content,
+            ContentHtml = Markdig.Markdown.ToHtml(post.Content, mdPipeline),
+            Path = post.Path,
+            CreationTime = post.CreationTime,
+            LastUpdateTime = post.LastUpdateTime,
+            Category = post.Category,
+            Categories = new List<Category>()
+        };
+
+        foreach (var itemId in post.Categories.Split(",").Select(int.Parse)) {
+            var item = _categoryRepo.Where(a => a.Id == itemId).First();
+            if (item != null) vm.Categories.Add(item);
+        }
+
+        return vm;
+    }
+}
