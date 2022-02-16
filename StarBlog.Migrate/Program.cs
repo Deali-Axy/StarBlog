e875d196b0d69ed2cab991d6bbc93120cf81a4f1
DealiAxy <dealiaxy@gmail.com>
2022-02-16 18:42:46 +0800

    升级了博客导入恒旭

--- StarBlog.Migrate/Program.cs
+++ StarBlog.Migrate/Program.cs
@@ -1,20 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using StarBlog.Contrib.Extensions;
 using StarBlog.Data;
 using StarBlog.Data.Models;
 using StarBlog.Migrate;
 
 var log = new System.Collections.Specialized.StringCollection();
 
-const string path = @"D:\blog\";
+const string path = @"E:\Documents\0_Write\0_blog";
 
 var exclusionDirs = new List<string> {".git"};
 
+// 删除旧文件
+var removeFileList = new List<string> {"app.db", "app.db-shm", "app.db-wal"};
+foreach (var filename in removeFileList.Where(filename => File.Exists(filename))) {
+    Console.WriteLine($"删除旧文件：{filename}");
+    File.Delete(filename);
+}
+
+// 数据库
 var freeSql = FreeSqlFactory.Create("Data Source=app.db;Synchronous=Off;Journal Mode=WAL;Cache Size=5000;");
 var postRepo = freeSql.GetRepository<Post>();
 var categoryRepo = freeSql.GetRepository<Category>();
 
+// 数据导入
 WalkDirectoryTree(new DirectoryInfo(path));
+
+// 覆盖数据库
+var destFile = Path.GetFullPath("../../../../StarBlog.Web/app.db");
+Console.WriteLine($"覆盖数据库：{destFile}");
+File.Copy("app.db", destFile, true);
 
 void WalkDirectoryTree(DirectoryInfo root) {
     FileInfo[] files = null;
@@ -37,62 +52,63 @@ void WalkDirectoryTree(DirectoryInfo root) {
         Console.WriteLine(e.Message);
     }
 
-    if (files != null) {
-        foreach (FileInfo fi in files) {
-            // In this example, we only access the existing FileInfo object. If we
-            // want to open, delete or modify the file, then
-            // a try-catch block is required here to handle the case
-            // where the file has been deleted since the call to TraverseTree().
-            Console.WriteLine(fi.FullName);
-            var postPath = fi.DirectoryName!.Replace(path, "");
+    if (files == null) return;
 
-            var categoryNames = postPath.Split("\\");
-            var categories = new List<Category>();
-            if (categoryNames.Length > 0) {
-                var rootCategory = categoryRepo.Where(a => a.Name == categoryNames[0]).First()
-                                   ?? categoryRepo.Insert(new Category {Name = categoryNames[0]});
-                categories.Add(rootCategory);
-                for (var i = 1; i < categoryNames.Length; i++) {
-                    var name = categoryNames[i];
-                    var parent = categories[i - 1];
-                    var category = categoryRepo.Where(
-                                       a => a.ParentId == parent.Id && a.Name == name).First()
-                                   ?? categoryRepo.Insert(new Category {
-                                       Name = name,
-                                       ParentId = parent.Id
-                                   });
-                    categories.Add(category);
-                }
+    foreach (var fi in files) {
+        // In this example, we only access the existing FileInfo object. If we
+        // want to open, delete or modify the file, then
+        // a try-catch block is required here to handle the case
+        // where the file has been deleted since the call to TraverseTree().
+        Console.WriteLine(fi.FullName);
+        var postPath = fi.DirectoryName!.Replace(path, "");
+
+        var categoryNames = postPath.Split("\\");
+        var categories = new List<Category>();
+        if (categoryNames.Length > 0) {
+            var rootCategory = categoryRepo.Where(a => a.Name == categoryNames[0]).First()
+                               ?? categoryRepo.Insert(new Category {Name = categoryNames[0]});
+            categories.Add(rootCategory);
+            for (var i = 1; i < categoryNames.Length; i++) {
+                var name = categoryNames[i];
+                var parent = categories[i - 1];
+                var category = categoryRepo.Where(
+                                   a => a.ParentId == parent.Id && a.Name == name).First()
+                               ?? categoryRepo.Insert(new Category {
+                                   Name = name,
+                                   ParentId = parent.Id
+                               });
+                categories.Add(category);
             }
-
-            var reader = fi.OpenText();
-            var content = reader.ReadToEnd();
-            reader.Close();
-
-            var post = new Post {
-                Id = GuidUtils.GuidTo16String(),
-                Title = fi.Name,
-                Content = content,
-                Path = postPath,
-                CategoryId = categories[^1].Id
-            };
-            postRepo.Insert(post);
         }
 
-        // Now find all the subdirectories under this directory.
-        subDirs = root.GetDirectories();
+        var reader = fi.OpenText();
+        var content = reader.ReadToEnd();
+        reader.Close();
 
-        foreach (DirectoryInfo dirInfo in subDirs) {
-            if (exclusionDirs.Contains(dirInfo.Name)) {
-                continue;
-            }
+        var post = new Post {
+            Id = GuidUtils.GuidTo16String(),
+            Title = fi.Name,
+            Summary = content.Limit(200),
+            Content = content,
+            Path = postPath,
+            CategoryId = categories[^1].Id
+        };
+        postRepo.Insert(post);
+    }
 
-            if (dirInfo.Name.EndsWith(".assets")) {
-                continue;
-            }
+    // Now find all the subdirectories under this directory.
+    subDirs = root.GetDirectories();
 
-            // Resursive call for each subdirectory.
-            WalkDirectoryTree(dirInfo);
+    foreach (DirectoryInfo dirInfo in subDirs) {
+        if (exclusionDirs.Contains(dirInfo.Name)) {
+            continue;
         }
+
+        if (dirInfo.Name.EndsWith(".assets")) {
+            continue;
+        }
+
+        // Resursive call for each subdirectory.
+        WalkDirectoryTree(dirInfo);
     }
 }
