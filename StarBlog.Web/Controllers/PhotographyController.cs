9bf899264b17b5daaba76ef98b8fefd7bf87f0dc
DealiAxy <dealiaxy@gmail.com>
2022-02-17 00:20:17 +0800

    增加摄影页面

---
+++ StarBlog.Web/Controllers/PhotographyController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarBlog.Web.Controllers; 
+
+public class PhotographyController : Controller {
+    // GET
+    public IActionResult Index() {
+        return View();
+    }
+}
