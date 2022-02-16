776bc4a1b88ad5178e9e3215c32e66c8f200a04a
DealiAxy <dealiaxy@gmail.com>
2022-02-17 00:20:40 +0800

    博客模块修改

--- StarBlog.Web/Controllers/BlogController.cs
+++ StarBlog.Web/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using FreeSql;
 using Microsoft.AspNetCore.Mvc;
 using StarBlog.Data.Models;
+using StarBlog.Web.Services;
 using StarBlog.Web.ViewModels;
 
 namespace StarBlog.Web.Controllers;
@@ -8,23 +9,23 @@ namespace StarBlog.Web.Controllers;
 public class BlogController : Controller {
     private readonly IBaseRepository<Post> _postRepo;
     private readonly IBaseRepository<Category> _categoryRepo;
+    private readonly PostService _postService;
 
-    public BlogController(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo) {
+    public BlogController(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo, PostService postService) {
         _postRepo = postRepo;
         _categoryRepo = categoryRepo;
+        _postService = postService;
     }
 
     public IActionResult List() {
-        return View(new BlogList {
+        return View(new BlogListViewModel {
             Posts = _postRepo.Select.Include(a => a.Category).ToList()
         });
     }
 
     public IActionResult Details(string id) {
-        return View(
-            _postRepo.Where(a => a.Id == id)
-                .Include(a => a.Category)
-                .First()
-        );
+        return View(_postService.GetPostViewModel(_postRepo.Where(a => a.Id == id)
+            .Include(a => a.Category)
+            .First()));
     }
 }
