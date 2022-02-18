7bfca713896264a9704f9fe4ede20b64ce3866a3
DealiAxy <dealiaxy@gmail.com>
2022-02-18 17:54:55 +0800

    加了个Api接口

---
+++ StarBlog.Web/Apis/BlogPostController.cs
@@ -0,0 +1,27 @@
+﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+
+namespace StarBlog.Web.Apis; 
+
+[ApiController]
+[Route("Api/[controller]")]
+public class BlogPostController : ControllerBase {
+    private readonly IBaseRepository<Post> _postRepo;
+
+    public BlogPostController(IBaseRepository<Post> postRepo) {
+        _postRepo = postRepo;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Post>> GetAll() {
+        return _postRepo.Select.Include(a => a.Category).ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Post> Get(string id) {
+        var post = _postRepo.Where(a => a.Id == id).First();
+        if (post == null) return NotFound();
+        return post;
+    }
+}
