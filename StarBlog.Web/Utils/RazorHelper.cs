b2da1c2242cd8b6527eb322c6e404488bb56d63f
DealiAxy <dealiaxy@gmail.com>
2022-02-19 00:51:59 +0800

    增加分页，数量统计功能

---
+++ StarBlog.Web/Utils/RazorHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StarBlog.Web.Utils; 
+
+public class RazorHelper {
+    public static string GetCurrentControllerName(ViewContext viewContext) {
+        return viewContext.RouteData.Values["controller"] == null ? "" : viewContext.RouteData.Values["controller"].ToString();
+    }
+
+    public static string GetCurrentActionName(ViewContext viewContext) {
+        return viewContext.RouteData.Values["action"] == null ? "" : viewContext.RouteData.Values["action"].ToString();
+    }
+}
