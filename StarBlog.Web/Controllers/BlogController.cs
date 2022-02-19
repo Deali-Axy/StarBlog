26142503b65bae5851e7cad8d621e13f504c5684
DealiAxy <dealiaxy@gmail.com>
2022-02-19 18:08:14 +0800

    随机文章加入提醒

--- StarBlog.Web/Controllers/BlogController.cs
+++ StarBlog.Web/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using FreeSql;
 using Microsoft.AspNetCore.Mvc;
+using StarBlog.Contrib.SiteMessage;
 using StarBlog.Data.Models;
 using StarBlog.Web.Services;
 using StarBlog.Web.ViewModels;
@@ -8,14 +9,17 @@ using X.PagedList;
 namespace StarBlog.Web.Controllers;
 
 public class BlogController : Controller {
+    private readonly Messages _messages;
     private readonly IBaseRepository<Post> _postRepo;
     private readonly IBaseRepository<Category> _categoryRepo;
     private readonly PostService _postService;
 
-    public BlogController(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo, PostService postService) {
+    public BlogController(IBaseRepository<Post> postRepo, IBaseRepository<Category> categoryRepo, PostService postService,
+        Messages messages) {
         _postRepo = postRepo;
         _categoryRepo = categoryRepo;
         _postService = postService;
+        _messages = messages;
     }
 
     public IActionResult List(int categoryId = 0, int page = 1, int pageSize = 5) {
@@ -52,6 +56,7 @@ public class BlogController : Controller {
     public IActionResult RandomPost() {
         var posts = _postRepo.Select.ToList();
         var rndPost = posts[new Random().Next(posts.Count)];
+        _messages.Info($"随机推荐了文章 <b>{rndPost.Title}</b> 给你~");
         return RedirectToAction(nameof(Data.Models.Post), new {id = rndPost.Id});
     }
 }
