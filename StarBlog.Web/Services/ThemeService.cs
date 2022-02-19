bf7005af21b116a9100ec6f47b774f7c3d3885b8
DealiAxy <dealiaxy@gmail.com>
2022-02-19 14:31:02 +0800

    终于实现主题切换功能~

---
+++ StarBlog.Web/Services/ThemeService.cs
@@ -0,0 +1,24 @@
+﻿namespace StarBlog.Web.Services;
+
+public class ThemeService {
+    private const string CssUrlPrefix = "/lib/bootswatch/dist";
+    private readonly string _themePath = Path.Combine("wwwroot", "lib", "bootswatch", "dist");
+    public List<Theme> Themes { get; set; } = new List<Theme>();
+
+    public ThemeService() {
+        foreach (var item in Directory.GetDirectories(_themePath)) {
+            var name = Path.GetFileName(item);
+            Themes.Add(new Theme {
+                Name = name,
+                Path = item,
+                CssUrl = $"{CssUrlPrefix}/{name}/bootstrap.min.css"
+            });
+        }
+    }
+}
+
+public class Theme {
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public string CssUrl { get; set; }
+}
---
+++ StarBlog.Web/wwwroot/js/site-preload.js
@@ -0,0 +1,15 @@
+﻿/**
+ * 动态加载CSS
+ * 参考：http://lengyun.github.io/js/3-2-2dynamicAddCSS.html
+ */
+function loadStyles(url) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(link);
+}
+
+let themeCssUrl = localStorage.getItem('currentThemeCssUrl')
+if (themeCssUrl != null) loadStyles(themeCssUrl)
