c7de5e6a37dc01916f584f687273d0803af666e6
DealiAxy <dealiaxy@gmail.com>
2022-02-19 14:31:02 +0800

    终于实现主题切换功能~

---
+++ StarBlog.Web/Apis/ThemeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarBlog.Web.Services;
+
+namespace StarBlog.Web.Apis; 
+
+[ApiController]
+[Route("Api/[controller]")]
+public class ThemeController : ControllerBase {
+    private readonly ThemeService _themeService;
+
+    public ThemeController(ThemeService themeService) {
+        _themeService = themeService;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Theme>> GetAll() {
+        return _themeService.Themes;
+    }
+}
---
+++ StarBlog.Web/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿let app = new Vue({
+    el: '#vue-header',
+    data: {
+        currentTheme: '',
+        themes: []
+    },
+    created: function () {
+        fetch('/Api/Theme')
+            .then(res => res.json())
+            .then(data => {
+                this.themes = data
+            })
+
+        // 读取本地主题配置
+        let theme = localStorage.getItem('currentTheme')
+        if (theme != null) this.currentTheme = theme
+    },
+    methods: {
+        setTheme(themeName) {
+            let theme = this.themes.find(t => t.name === themeName)
+            loadStyles(theme.cssUrl)
+            this.currentTheme = themeName
+            localStorage.setItem('currentTheme', themeName)
+            localStorage.setItem('currentThemeCssUrl', theme.cssUrl)
+        }
+    }
+})
