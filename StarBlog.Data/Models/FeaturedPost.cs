c8d0822271b895aeb9a80e9bb16a9301840fc641
DealiAxy <dealiaxy@gmail.com>
2022-02-19 17:28:58 +0800

    增加三个新模型

---
+++ StarBlog.Data/Models/FeaturedPost.cs
@@ -0,0 +1,10 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace StarBlog.Data.Models;
+
+public class FeaturedPost {
+    [Column(IsIdentity = true, IsPrimary = true)]
+    public int Id { get; set; }
+    public string PostId { get; set; }
+    public Post Post { get; set; }
+}
