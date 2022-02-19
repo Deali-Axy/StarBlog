703a583ca52eca004ca4964784b79d7205ec8ba1
DealiAxy <dealiaxy@gmail.com>
2022-02-19 17:28:58 +0800

    增加三个新模型

---
+++ StarBlog.Data/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace StarBlog.Data.Models; 
+
+public class Photo {
+    [Column(IsIdentity = false, IsPrimary = true)]
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Location { get; set; }
+    public DateTime CreateTime { get; set; }
+}
