3baadf635b3607d7e45ea14253b929709c482171
DealiAxy <dealiaxy@gmail.com>
2022-02-19 11:27:37 +0800

    完成了博客搜索功能~！

---
+++ StarBlog.Web/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,8 @@
+﻿using StarBlog.Data.Models;
+
+namespace StarBlog.Web.ViewModels; 
+
+public class SearchResultViewModel {
+    public string Keyword { get; set; }
+    public List<Post> Posts { get; set; }
+}
