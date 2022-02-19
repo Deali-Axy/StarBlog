4257c8db9f3ee2a365f58fbf9252b23560185299
DealiAxy <dealiaxy@gmail.com>
2022-02-19 14:31:32 +0800

    RazorHelper.cs 优化

--- StarBlog.Web/Utils/RazorHelper.cs
+++ StarBlog.Web/Utils/RazorHelper.cs
@@ -4,10 +4,10 @@ namespace StarBlog.Web.Utils;
 
 public class RazorHelper {
     public static string GetCurrentControllerName(ViewContext viewContext) {
-        return viewContext.RouteData.Values["controller"] == null ? "" : viewContext.RouteData.Values["controller"].ToString();
+        return viewContext.RouteData.Values["controller"]?.ToString() ?? "Home";
     }
 
     public static string GetCurrentActionName(ViewContext viewContext) {
-        return viewContext.RouteData.Values["action"] == null ? "" : viewContext.RouteData.Values["action"].ToString();
+        return viewContext.RouteData.Values["action"]?.ToString() ?? "Index";
     }
 }
