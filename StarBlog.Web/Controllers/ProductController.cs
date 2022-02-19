b00d5a0bbd581791ffa423fb63ce27b026855238
DealiAxy <dealiaxy@gmail.com>
2022-02-20 00:02:36 +0800

    增加了products页面

---
+++ StarBlog.Web/Controllers/ProductController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarBlog.Web.Controllers; 
+
+public class ProductController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+}
