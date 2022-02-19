c45651ff22b272f902cde8e540dded75a753ba38
DealiAxy <dealiaxy@gmail.com>
2022-02-19 17:34:22 +0800

    添加照片接口

---
+++ StarBlog.Web/Apis/PhotoController.cs
@@ -0,0 +1,30 @@
+﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+
+namespace StarBlog.Web.Apis;
+
+/// <summary>
+/// 摄影
+/// </summary>
+[ApiController]
+[Route("Api/[controller]")]
+public class PhotoController : ControllerBase {
+    private readonly IBaseRepository<Photo> _photoRepo;
+
+    public PhotoController(IBaseRepository<Photo> photoRepo) {
+        _photoRepo = photoRepo;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Photo>> GetAll() {
+        return _photoRepo.Select.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Photo> Get(string id) {
+        var photo = _photoRepo.Where(a => a.Id == id).First();
+        if (photo == null) return NotFound();
+        return photo;
+    }
+}
