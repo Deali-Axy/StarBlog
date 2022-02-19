3ac8410e86e8e23c268ad3301a635483db87794f
DealiAxy <dealiaxy@gmail.com>
2022-02-19 17:29:13 +0800

    实现首页博客推荐功能

--- StarBlog.Web/Controllers/HomeController.cs
+++ StarBlog.Web/Controllers/HomeController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+using StarBlog.Web.Services;
+using StarBlog.Web.ViewModels;
 
-namespace StarBlog.Web.Controllers; 
+namespace StarBlog.Web.Controllers;
 
 public class HomeController : Controller {
-    // GET
+    private readonly IBaseRepository<Post> _postRepo;
+    private readonly BlogService _blogService;
+
+    public HomeController(IBaseRepository<Post> postRepo, BlogService blogService) {
+        _postRepo = postRepo;
+        _blogService = blogService;
+    }
+
     public IActionResult Index() {
-        return View();
+        return View(new HomeViewModel {
+            TopPost = _blogService.GetTopOnePost(),
+            FeaturedPosts = _blogService.GetFeaturedPosts()
+        });
     }
 }
