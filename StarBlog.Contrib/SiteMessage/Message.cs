741c9e7823cebc3b52c9f7100e8120564b2862e7
DealiAxy <dealiaxy@gmail.com>
2022-02-19 18:08:04 +0800

    引入消息框架

---
+++ StarBlog.Contrib/SiteMessage/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace StarBlog.Contrib.SiteMessage; 
+
+public class Message {
+    public string Tag { get; set; }
+    public string Content { get; set; }
+}
+
+public static class MessageTags {
+    public const string Debug = "secondary";
+    public const string Info = "info";
+    public const string Success = "success";
+    public const string Warning = "warning";
+    public const string Error = "danger";
+}
---
+++ StarBlog.Contrib/SiteMessage/Messages.cs
@@ -0,0 +1,44 @@
+﻿namespace StarBlog.Contrib.SiteMessage; 
+
+public class Messages {
+    private Queue<Message> MessagesQueue { get; set; } = new Queue<Message>();
+
+    public bool IsEmpty => MessagesQueue.Count == 0;
+
+    public Message Dequeue() => MessagesQueue.Dequeue();
+
+    public void Debug(string content) {
+        MessagesQueue.Enqueue(new Message {
+            Tag = MessageTags.Debug,
+            Content = content
+        });
+    }
+
+    public void Success(string content) {
+        MessagesQueue.Enqueue(new Message {
+            Tag = MessageTags.Success,
+            Content = content
+        });
+    }
+
+    public void Info(string content) {
+        MessagesQueue.Enqueue(new Message {
+            Tag = MessageTags.Info,
+            Content = content
+        });
+    }
+
+    public void Warning(string content) {
+        MessagesQueue.Enqueue(new Message {
+            Tag = MessageTags.Warning,
+            Content = content
+        });
+    }
+
+    public void Error(string content) {
+        MessagesQueue.Enqueue(new Message {
+            Tag = MessageTags.Error,
+            Content = content
+        });
+    }
+}
