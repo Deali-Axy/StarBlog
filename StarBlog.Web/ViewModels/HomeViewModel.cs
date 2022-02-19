9265eeb5b74679f6c969caf19520ce0ff2001dac
DealiAxy <dealiaxy@gmail.com>
2022-02-19 17:28:21 +0800

    首页继续优化

---
+++ StarBlog.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using StarBlog.Data.Models;
+
+namespace StarBlog.Web.ViewModels;
+
+public class HomeViewModel {
+    /// <summary>
+    /// 置顶博客
+    /// </summary>
+    public Post? TopPost { get; set; }
+
+    /// <summary>
+    /// 推荐博客行，一行最多两个博客
+    /// </summary>
+    public List<List<Post>>? FeaturedPosts { get; set; }
+}
