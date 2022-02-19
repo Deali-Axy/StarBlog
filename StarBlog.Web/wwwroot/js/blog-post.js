35d0c9b0b52aa99935fca4a7b634cd0a80b467ac
DealiAxy <dealiaxy@gmail.com>
2022-02-19 14:44:01 +0800

    博客文章显示图片的问题也完美搞定了~

---
+++ StarBlog.Web/wwwroot/js/blog-post.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    let testEditormdView = editormd.markdownToHTML("test-editormd-view", {
+        htmlDecode: "style,script,iframe",  // you can filter tags decode
+        //toc             : false,
+        tocm: true,    // Using [TOCM]
+        tocContainer: "#custom-toc-container", // 自定义 ToC 容器层
+        //gfm             : false,
+        //tocDropdown     : true,
+        // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
+        emoji: true,
+        taskList: true,
+        tex: true,  // 默认不解析
+        flowChart: true,  // 默认不解析
+        sequenceDiagram: true,  // 默认不解析
+    });
+})
+
+function procImages(postId) {
+    $.get(`/Api/BlogPost/${postId}/`, function (res) {
+        for (const imgElem of document.querySelectorAll('.post-content img')) {
+            let originSrc = imgElem.getAttribute('src')
+            let newSrc = `/assets/blog/${res.path}/${originSrc}`
+            console.log('originSrc', originSrc)
+            console.log('newSrc', newSrc)
+            imgElem.setAttribute('src', newSrc)
+        }
+    })
+}
