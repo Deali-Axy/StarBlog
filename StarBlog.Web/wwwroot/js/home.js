ad1f21afec65f4391a731add0994274f4c84dfa5
DealiAxy <dealiaxy@gmail.com>
2022-02-19 18:00:10 +0800

    给首页增加随机诗词和一言功能

---
+++ StarBlog.Web/wwwroot/js/home.js
@@ -0,0 +1,22 @@
+﻿let homeApp = new Vue({
+    el: '#vue-app',
+    data: {
+        poem: {},
+        hitokoto: {},
+        poemSimple: ''
+    },
+    created: function () {
+        fetch('http://dc.sblt.deali.cn:9800/poem/simple')
+            .then(res => res.text()).then(data => this.poemSimple = data)
+        fetch('http://dc.sblt.deali.cn:9800/poem/tang')
+            .then(res => res.json())
+            .then(data => {
+                this.poem = data.data
+            })
+        fetch('http://dc.sblt.deali.cn:9800/hitokoto/get')
+            .then(res => res.json())
+            .then(data => {
+                this.hitokoto = data.data[0]
+            })
+    }
+})
