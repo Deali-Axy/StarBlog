7151fe693ecb076f57e04a7e43df02f310247728
DealiAxy <dealiaxy@gmail.com>
2022-02-22 00:41:30 +0800

    博客接口做了分页功能

--- StarBlog.Web/Apis/BlogPostController.cs
+++ StarBlog.Web/Apis/BlogPostController.cs
@@ -1,6 +1,10 @@
 ﻿using FreeSql;
 using Microsoft.AspNetCore.Mvc;
 using StarBlog.Data.Models;
+using StarBlog.Web.Extensions;
+using StarBlog.Web.Services;
+using StarBlog.Web.ViewModels;
+using X.PagedList;
 
 namespace StarBlog.Web.Apis;
 
@@ -11,16 +15,23 @@ namespace StarBlog.Web.Apis;
 [Route("Api/[controller]")]
 public class BlogPostController : ControllerBase {
     private readonly IBaseRepository<Post> _postRepo;
+    private readonly PostService _postService;
 
-
-    public BlogPostController(IBaseRepository<Post> postRepo, IBaseRepository<TopPost> topPostRepo,
-        IBaseRepository<FeaturedPost> featuredPostRepo) {
+    public BlogPostController(IBaseRepository<Post> postRepo, PostService postService) {
         _postRepo = postRepo;
+        _postService = postService;
     }
 
     [HttpGet]
-    public ActionResult<List<Post>> GetAll() {
-        return _postRepo.Select.Include(a => a.Category).ToList();
+    public ActionResult<PagedResponse<Post>> GetList(int categoryId = 0, int page = 1, int pageSize = 10) {
+        var pagedList = _postService.GetPagedList(categoryId, page, pageSize);
+        var pagedRes = new PagedResponse<Post> {
+            Successful = true,
+            Message = "Get posts list",
+            Data = pagedList.ToList(),
+            Pagination = pagedList.ToPaginationMetadata()
+        };
+        return Ok(pagedRes);
     }
 
     [HttpGet("{id}")]
