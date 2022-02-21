d2603d3a6a94b95e7bd057c6bccc5abead4b64fe
DealiAxy <dealiaxy@gmail.com>
2022-02-22 00:41:00 +0800

    增加分页功能

---
+++ StarBlog.Web/ViewModels/PagedResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace StarBlog.Web.ViewModels;
+
+public class PagedResponse<T> where T : class {
+    public bool Successful { get; set; }
+    public string? Message { get; set; }
+    public PaginationMetadata? Pagination { get; set; }
+    public List<T> Data { get; set; }
+}
