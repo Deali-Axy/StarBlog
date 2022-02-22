e648abff7b14e5ae3ec4481cf5dd9532d82b69e7
DealiAxy <dealiaxy@gmail.com>
2022-02-22 12:13:18 +0800

    增加dto

---
+++ StarBlog.Web/Dtos/PostDtos.cs
@@ -0,0 +1,15 @@
+﻿using StarBlog.Data.Models;
+
+namespace StarBlog.Web.Dtos; 
+
+public class PostListDto {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Summary { get; set; }
+    public string Path { get; set; }
+    public DateTime CreationTime { get; set; }
+    public DateTime LastUpdateTime { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public string Categories { get; set; }
+}
