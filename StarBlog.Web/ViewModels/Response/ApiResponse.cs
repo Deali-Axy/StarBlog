dbe1e1b89cae4bee6581e0c665bd4df2eb719a80
DealiAxy <dealiaxy@gmail.com>
2022-02-22 18:47:30 +0800

    总算通过隐式转换的方式搞定了Response包装

--- StarBlog.Web/ViewModels/Response/ApiResponse.cs
+++ StarBlog.Web/ViewModels/Response/ApiResponse.cs
@@ -1,25 +1,44 @@
 ﻿namespace StarBlog.Web.ViewModels.Response;
 
-public class ApiResponse {
-    public bool Successful { get; set; } = true;
+public class ApiResponse<T> : IApiResponse<T> {
+    public bool Successful { get; set; }
     public string? Message { get; set; }
+    public T? Data { get; set; }
 
-    public static ApiResponse Ok(HttpResponse httpResponse, string? message = null) {
-        httpResponse.StatusCode = StatusCodes.Status200OK;
-        return new ApiResponse {Successful = true, Message = message};
-    }
-
-    public static ApiResponse NotFound(HttpResponse httpResponse) {
-        httpResponse.StatusCode = StatusCodes.Status404NotFound;
-        return new ApiResponse {Successful = false, Message = "Not found."};
+    /// <summary>
+    /// 实现将 <see cref="ApiResponse"/> 隐式转换为 <see cref="ApiResponse{T}"/>
+    /// </summary>
+    /// <param name="apiResponse"><see cref="ApiResponse"/></param>
+    /// <returns></returns>
+    public static implicit operator ApiResponse<T>(ApiResponse apiResponse) {
+        return new ApiResponse<T> {
+            Successful = apiResponse.Successful,
+            Message = apiResponse.Message
+        };
     }
 }
 
-public class ApiResponse<T> : ApiResponse where T : class {
-    public T? Data { get; set; }
+public class ApiResponse : IApiResponse {
+    public bool Successful { get; set; }
+    public string? Message { get; set; }
 
-    public new static ApiResponse<T> NotFound(HttpResponse httpResponse) {
+    public static ApiResponse Ok(HttpResponse httpResponse, string message = "Ok") {
+        httpResponse.StatusCode = StatusCodes.Status200OK;
+        return new ApiResponse { Successful = true, Message = message };
+    }
+
+    public static ApiResponse Unauthorized(HttpResponse httpResponse, string message = "Unauthorized") {
+        httpResponse.StatusCode = StatusCodes.Status401Unauthorized;
+        return new ApiResponse { Successful = false, Message = message };
+    }
+
+    public static ApiResponse NotFound(HttpResponse httpResponse, string message = "NotFound") {
         httpResponse.StatusCode = StatusCodes.Status404NotFound;
-        return new ApiResponse<T> {Successful = false, Message = "Not found."};
+        return new ApiResponse { Successful = false, Message = message };
+    }
+
+    public static ApiResponse BadRequest(HttpResponse httpResponse, string message = "BadRequest") {
+        httpResponse.StatusCode = StatusCodes.Status400BadRequest;
+        return new ApiResponse { Successful = false, Message = message };
     }
 }
