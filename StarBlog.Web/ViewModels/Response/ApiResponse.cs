925a7f6efbc3d78cb429fb435a00112d65ba0422
DealiAxy <dealiaxy@gmail.com>
2022-02-22 12:13:57 +0800

    response改个名，免得混淆了

---
+++ StarBlog.Web/ViewModels/Response/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace StarBlog.Web.ViewModels.Response;
+
+public class ApiResponse {
+    public bool Successful { get; set; } = true;
+    public string? Message { get; set; }
+
+    public static ApiResponse Ok(HttpResponse httpResponse, string? message = null) {
+        httpResponse.StatusCode = StatusCodes.Status200OK;
+        return new ApiResponse {Successful = true, Message = message};
+    }
+
+    public static ApiResponse NotFound(HttpResponse httpResponse) {
+        httpResponse.StatusCode = StatusCodes.Status404NotFound;
+        return new ApiResponse {Successful = false, Message = "Not found."};
+    }
+}
+
+public class ApiResponse<T> : ApiResponse where T : class {
+    public T? Data { get; set; }
+
+    public new static ApiResponse<T> NotFound(HttpResponse httpResponse) {
+        httpResponse.StatusCode = StatusCodes.Status404NotFound;
+        return new ApiResponse<T> {Successful = false, Message = "Not found."};
+    }
+}
