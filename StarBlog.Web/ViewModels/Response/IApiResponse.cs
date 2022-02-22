df1faaae9974070591cc232a79570e0a7ab576da
DealiAxy <dealiaxy@gmail.com>
2022-02-22 18:47:30 +0800

    总算通过隐式转换的方式搞定了Response包装

---
+++ StarBlog.Web/ViewModels/Response/IApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace StarBlog.Web.ViewModels.Response;
+
+public interface IApiResponse {
+    public bool Successful { get; set; }
+    public string? Message { get; set; }
+}
+
+public interface IApiResponse<T> : IApiResponse {
+    public T? Data { get; set; }
+}
