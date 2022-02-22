e64f2cc1de09e9f7fb8cb9f1fbdc4eded0bc02a5
DealiAxy <dealiaxy@gmail.com>
2022-02-22 22:07:26 +0800

    ApiResponse.cs 增加了状态码字段

--- StarBlog.Web/ViewModels/Response/ApiResponse.cs
+++ StarBlog.Web/ViewModels/Response/ApiResponse.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace StarBlog.Web.ViewModels.Response;
 
 public class ApiResponse<T> : IApiResponse<T> {
+    public int StatusCode { get; set; } = 200;
     public bool Successful { get; set; }
     public string? Message { get; set; }
     public T? Data { get; set; }
@@ -22,39 +23,56 @@ public class ApiResponse<T> : IApiResponse<T> {
 }
 
 public class ApiResponse : IApiResponse {
+    public int StatusCode { get; set; } = 200;
     public bool Successful { get; set; }
     public string? Message { get; set; }
 
     public static ApiResponse Ok(HttpResponse httpResponse, string message = "Ok") {
         httpResponse.StatusCode = StatusCodes.Status200OK;
-        return new ApiResponse {Successful = true, Message = message};
+        return new ApiResponse {
+            StatusCode = httpResponse.StatusCode,
+            Successful = true, Message = message
+        };
     }
 
     public static ApiResponse Unauthorized(HttpResponse httpResponse, string message = "Unauthorized") {
         httpResponse.StatusCode = StatusCodes.Status401Unauthorized;
-        return new ApiResponse {Successful = false, Message = message};
+        return new ApiResponse {
+            StatusCode = httpResponse.StatusCode,
+            Successful = false, Message = message
+        };
     }
 
     public static ApiResponse NotFound(HttpResponse httpResponse, string message = "NotFound") {
         httpResponse.StatusCode = StatusCodes.Status404NotFound;
-        return new ApiResponse {Successful = false, Message = message};
+        return new ApiResponse {
+            StatusCode = httpResponse.StatusCode,
+            Successful = false, Message = message
+        };
     }
 
     public static ApiResponse BadRequest(HttpResponse httpResponse, string message = "BadRequest") {
         httpResponse.StatusCode = StatusCodes.Status400BadRequest;
-        return new ApiResponse {Successful = false, Message = message};
+        return new ApiResponse {
+            StatusCode = httpResponse.StatusCode,
+            Successful = false, Message = message
+        };
     }
 
     public static ApiResponse<SerializableError> BadRequest(HttpResponse httpResponse
         , string message, ModelStateDictionary modelState) {
         httpResponse.StatusCode = StatusCodes.Status400BadRequest;
         return new ApiResponse<SerializableError> {
+            StatusCode = httpResponse.StatusCode,
             Successful = false, Message = message, Data = new SerializableError(modelState)
         };
     }
 
     public static ApiResponse Error(HttpResponse httpResponse, string message = "Error") {
         httpResponse.StatusCode = StatusCodes.Status500InternalServerError;
-        return new ApiResponse {Successful = false, Message = message};
+        return new ApiResponse {
+            StatusCode = httpResponse.StatusCode,
+            Successful = false, Message = message
+        };
     }
 }
