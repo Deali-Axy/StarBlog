ac452b10b1ebcf9874ae05cbe90ab4036b2c6f53
DealiAxy <dealiaxy@gmail.com>
2022-02-22 22:07:26 +0800

    ApiResponse.cs 增加了状态码字段

--- StarBlog.Web/ViewModels/Response/IApiResponse.cs
+++ StarBlog.Web/ViewModels/Response/IApiResponse.cs
@@ -1,6 +1,7 @@
 ﻿namespace StarBlog.Web.ViewModels.Response;
 
 public interface IApiResponse {
+    public int StatusCode { get; set; }
     public bool Successful { get; set; }
     public string? Message { get; set; }
 }
