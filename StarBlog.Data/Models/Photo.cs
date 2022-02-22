b3446978791e26eaf06e088553a860a7bc432b22
DealiAxy <dealiaxy@gmail.com>
2022-02-22 18:46:46 +0800

    Photo.cs 模型修改

--- StarBlog.Data/Models/Photo.cs
+++ StarBlog.Data/Models/Photo.cs
@@ -1,11 +1,23 @@
 ﻿using FreeSql.DataAnnotations;
 
-namespace StarBlog.Data.Models; 
+namespace StarBlog.Data.Models;
 
 public class Photo {
     [Column(IsIdentity = false, IsPrimary = true)]
     public string Id { get; set; }
+
+    /// <summary>
+    /// 作品标题
+    /// </summary>
     public string Title { get; set; }
+
+    /// <summary>
+    /// 文件存储位置
+    /// </summary>
     public string Location { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
     public DateTime CreateTime { get; set; }
 }
