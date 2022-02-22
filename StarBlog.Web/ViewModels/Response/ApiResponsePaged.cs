2bd4218eb995f54c9fed2baf9eddf58c0ad632db
DealiAxy <dealiaxy@gmail.com>
2022-02-22 12:13:57 +0800

    response改个名，免得混淆了

---
+++ StarBlog.Web/ViewModels/Response/ApiResponsePaged.cs
@@ -0,0 +1,5 @@
+﻿namespace StarBlog.Web.ViewModels.Response;
+
+public class ApiResponsePaged<T> : ApiResponse<List<T>> where T : class {
+    public PaginationMetadata? Pagination { get; set; }
+}
--- StarBlog.Web/ViewModels/ResponsePaged.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace StarBlog.Web.ViewModels;
-
-public class ResponsePaged<T> : Response<List<T>> where T : class {
-    public PaginationMetadata? Pagination { get; set; }
-}
