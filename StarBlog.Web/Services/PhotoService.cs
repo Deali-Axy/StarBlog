df20073d209c33cd42732cefd95c655a27a6f75c
DealiAxy <dealiaxy@gmail.com>
2022-02-22 18:47:53 +0800

    photo操作包装

---
+++ StarBlog.Web/Services/PhotoService.cs
@@ -0,0 +1,25 @@
+﻿using FreeSql;
+using StarBlog.Data.Models;
+using X.PagedList;
+
+namespace StarBlog.Web.Services;
+
+public class PhotoService {
+    private readonly IBaseRepository<Photo> _photoRepo;
+
+    public PhotoService(IBaseRepository<Photo> photoRepo) {
+        _photoRepo = photoRepo;
+    }
+
+    public IPagedList<Photo> GetPagedList(int page = 1, int pageSize = 10) {
+        return _photoRepo.Select.ToList().ToPagedList(page, pageSize);
+    }
+
+    public Photo? GetById(string id) {
+        return _photoRepo.Where(a => a.Id == id).First();
+    }
+
+    public int DeleteById(string id) {
+        return _photoRepo.Delete(a => a.Id == id);
+    }
+}
