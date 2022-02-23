4575a32e273291df41a152b3eecc2c93604b72cc
DealiAxy <dealiaxy@gmail.com>
2022-02-23 21:01:08 +0800

    增加用户模型

---
+++ StarBlog.Data/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace StarBlog.Data.Models; 
+
+public class User {
+    [Column(IsIdentity = false, IsPrimary = true)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+}
