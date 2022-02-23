11d95c424cece334f4e6caddb465c4d1c7c63e8d
DealiAxy <dealiaxy@gmail.com>
2022-02-23 21:00:48 +0800

    开启swagger小绿锁

---
+++ StarBlog.Web/Extensions/ConfigureSwagger.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace StarBlog.Web.Extensions;
+
+public static class ConfigureSwagger {
+    public static void AddSwagger(this IServiceCollection services) {
+        services.AddSwaggerGen(options => {
+            var security = new OpenApiSecurityScheme {
+                Description = "JWT模式授权，请输入 Bearer {Token} 进行身份验证",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = ""
+            };
+            options.AddSecurityDefinition("oauth2", security);
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement { { security, new List<string>() } });
+            options.OperationFilter<AddResponseHeadersFilter>();
+            options.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+            options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+            var filePath = Path.Combine(System.AppContext.BaseDirectory, $"{typeof(Program).Assembly.GetName().Name}.xml");
+            options.IncludeXmlComments(filePath, true);
+        });
+    }
+}
