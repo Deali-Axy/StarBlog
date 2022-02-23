90076ec332c00aa584dee2ebe113fe9d91626d44
DealiAxy <dealiaxy@gmail.com>
2022-02-23 21:14:29 +0800

    写了配置模型

---
+++ StarBlog.Web/Models/Config/SecuritySettings.cs
@@ -0,0 +1,11 @@
+﻿namespace StarBlog.Web.Models.Config; 
+
+public class SecuritySettings {
+    public Token Token { get; set; }
+}
+
+public class Token {
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string Key { get; set; }
+}
