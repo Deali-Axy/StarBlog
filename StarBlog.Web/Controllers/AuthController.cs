497908870e563939093264dee459dabab8552a3f
DealiAxy <dealiaxy@gmail.com>
2022-02-23 23:40:05 +0800

    完成认证功能

---
+++ StarBlog.Web/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+using StarBlog.Web.Services;
+using StarBlog.Web.ViewModels;
+using StarBlog.Web.ViewModels.Response;
+
+namespace StarBlog.Web.Controllers;
+
+/// <summary>
+/// 认证
+/// </summary>
+[ApiController]
+[Route("Api/[controller]")]
+public class AuthController : ControllerBase {
+    private readonly AuthService _authService;
+
+    public AuthController(AuthService authService) {
+        _authService = authService;
+    }
+
+    /// <summary>
+    /// 登录
+    /// </summary>
+    /// <param name="loginUser"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public ApiResponse<LoginToken> Login(LoginUser loginUser) {
+        var user = _authService.GetUserByName(loginUser.Username);
+        if (user == null) return ApiResponse.NotFound(Response);
+        if (loginUser.Password != user.Password) return ApiResponse.Unauthorized(Response);
+        return new ApiResponse<LoginToken>(_authService.GenerateLoginToken(user));
+    }
+
+    /// <summary>
+    /// 获取当前用户信息
+    /// </summary>
+    /// <returns></returns>
+    [Authorize]
+    [HttpGet]
+    public ActionResult<User> GetUser() {
+        var user = _authService.GetUser(User);
+        if (user == null) return NotFound();
+        return user;
+    }
+}
---
+++ StarBlog.Web/Extensions/ConfigureAuth.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using StarBlog.Web.Models.Config;
+using StarBlog.Web.Services;
+
+namespace StarBlog.Web.Extensions; 
+
+public static class ConfigureAuth {
+    public static void AddAuth(this IServiceCollection services, IConfiguration configuration) {
+        services.AddScoped<AuthService>();
+        services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options => {
+                var secSettings = configuration.GetSection(nameof(SecuritySettings)).Get<SecuritySettings>();
+                options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = secSettings.Token.Issuer,
+                    ValidAudience = secSettings.Token.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secSettings.Token.Key)),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+    }
+}
