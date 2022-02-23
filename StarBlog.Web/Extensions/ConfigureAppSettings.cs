2d7150632952f70a14feb5c0c86a0f1d14191205
DealiAxy <dealiaxy@gmail.com>
2022-02-23 21:14:29 +0800

    写了配置模型

---
+++ StarBlog.Web/Extensions/ConfigureAppSettings.cs
@@ -0,0 +1,10 @@
+﻿using StarBlog.Web.Models.Config;
+
+namespace StarBlog.Web.Extensions; 
+
+public static class ConfigureAppSettings {
+    public static void AddSettings(this IServiceCollection services, IConfiguration configuration) {
+        // 安全配置
+        services.Configure<SecuritySettings>(configuration.GetSection(nameof(SecuritySettings)));
+    }
+}
