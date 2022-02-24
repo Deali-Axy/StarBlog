3bca7be30ba4b1b556fb164ef5e537c5d448c5d7
DealiAxy <dealiaxy@gmail.com>
2022-02-24 23:53:52 +0800

    修改swagger小绿锁提示

--- StarBlog.Web/Extensions/ConfigureSwagger.cs
+++ StarBlog.Web/Extensions/ConfigureSwagger.cs
@@ -7,11 +7,10 @@ public static class ConfigureSwagger {
     public static void AddSwagger(this IServiceCollection services) {
         services.AddSwaggerGen(options => {
             var security = new OpenApiSecurityScheme {
-                Description = "JWT模式授权，请输入 Bearer {Token} 进行身份验证",
+                Description = "JWT模式授权，请输入 \"Bearer {Token}\" 进行身份验证",
                 Name = "Authorization",
                 In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey,
-                BearerFormat = ""
+                Type = SecuritySchemeType.ApiKey
             };
             options.AddSecurityDefinition("oauth2", security);
             options.AddSecurityRequirement(new OpenApiSecurityRequirement { { security, new List<string>() } });
