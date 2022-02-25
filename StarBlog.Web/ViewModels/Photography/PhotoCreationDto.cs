135c63e8ae8c7605879cd3971d0c8426078dc34a
DealiAxy <dealiaxy@gmail.com>
2022-02-25 18:24:45 +0800

    图片创建api完善

---
+++ StarBlog.Web/ViewModels/Photography/PhotoCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StarBlog.Web.ViewModels.Photography;
+
+public class PhotoCreationDto {
+    /// <summary>
+    /// 作品标题
+    /// </summary>
+    [Required(ErrorMessage = "作品标题不能为空")]
+    public string Title { get; set; }
+
+    /// <summary>
+    /// 拍摄地点
+    /// </summary>
+    [Required(ErrorMessage = "拍摄地点不能为空")]
+    public string Location { get; set; }
+}
