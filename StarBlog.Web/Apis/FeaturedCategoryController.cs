1bd0f2e07397c56a6ea0390ccab9001c8e0ba63c
DealiAxy <dealiaxy@gmail.com>
2022-02-26 20:25:48 +0800

    修改三个接口

---
+++ StarBlog.Web/Apis/FeaturedCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+using StarBlog.Web.Services;
+using StarBlog.Web.ViewModels.Response;
+
+namespace StarBlog.Web.Apis;
+
+/// <summary>
+/// 推荐分类
+/// </summary>
+[ApiController]
+[Route("Api/[controller]")]
+public class FeaturedCategoryController : ControllerBase {
+    private readonly CategoryService _categoryService;
+
+    public FeaturedCategoryController(CategoryService categoryService) {
+        _categoryService = categoryService;
+    }
+
+    [HttpPost]
+    public ApiResponse<List<FeaturedCategory>> GetList() {
+        return new ApiResponse<List<FeaturedCategory>>(_categoryService.GetFeaturedCategories());
+    }
+}
---
+++ StarBlog.Web/Apis/FeaturedPhotoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+using StarBlog.Web.Services;
+using StarBlog.Web.ViewModels.Response;
+
+namespace StarBlog.Web.Apis;
+
+/// <summary>
+/// 推荐图片
+/// </summary>
+[ApiController]
+[Route("Api/[controller]")]
+public class FeaturedPhotoController : ControllerBase {
+    private readonly PhotoService _photoService;
+
+    public FeaturedPhotoController(PhotoService photoService) {
+        _photoService = photoService;
+    }
+
+    [HttpGet]
+    public ApiResponse<List<Photo>> GetList() {
+        return new ApiResponse<List<Photo>>(_photoService.GetFeaturedPhotos());
+    }
+}
