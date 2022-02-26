41c554120e12651607f29f6031b9992c3ea43f88
DealiAxy <dealiaxy@gmail.com>
2022-02-26 20:26:27 +0800

    主页修改

--- StarBlog.Web/Controllers/HomeController.cs
+++ StarBlog.Web/Controllers/HomeController.cs
@@ -1,24 +1,26 @@
-﻿using FreeSql;
-using Microsoft.AspNetCore.Mvc;
-using StarBlog.Data.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using StarBlog.Web.Services;
 using StarBlog.Web.ViewModels;
 
 namespace StarBlog.Web.Controllers;
 
 public class HomeController : Controller {
-    private readonly IBaseRepository<Post> _postRepo;
     private readonly BlogService _blogService;
+    private readonly PhotoService _photoService;
+    private readonly CategoryService _categoryService;
 
-    public HomeController(IBaseRepository<Post> postRepo, BlogService blogService) {
-        _postRepo = postRepo;
+    public HomeController(BlogService blogService, PhotoService photoService, CategoryService categoryService) {
         _blogService = blogService;
+        _photoService = photoService;
+        _categoryService = categoryService;
     }
 
     public IActionResult Index() {
         return View(new HomeViewModel {
             TopPost = _blogService.GetTopOnePost(),
-            FeaturedPosts = _blogService.GetFeaturedPostRows()
+            FeaturedPosts = _blogService.GetFeaturedPostRows(),
+            FeaturedPhotos = _photoService.GetFeaturedPhotos(),
+            FeaturedCategories = _categoryService.GetFeaturedCategories()
         });
     }
 }
--- StarBlog.Web/ViewModels/HomeViewModel.cs
+++ StarBlog.Web/ViewModels/HomeViewModel.cs
@@ -12,4 +12,14 @@ public class HomeViewModel {
     /// 推荐博客行，一行最多两个博客
     /// </summary>
     public List<List<Post>> FeaturedPosts { get; set; } = new();
+
+    /// <summary>
+    /// 推荐照片，原则上只能三个
+    /// </summary>
+    public List<Photo> FeaturedPhotos { get; set; } = new();
+
+    /// <summary>
+    /// 推荐分类，原则上三个
+    /// </summary>
+    public List<FeaturedCategory> FeaturedCategories { get; set; } = new();
 }
