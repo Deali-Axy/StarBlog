7823b7a1c01d2486d61d0ac9376e0295f1b20c37
DealiAxy <dealiaxy@gmail.com>
2022-02-26 20:25:31 +0800

    增加推荐分类

---
+++ StarBlog.Data/Models/FeaturedPhoto.cs
@@ -0,0 +1,11 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace StarBlog.Data.Models;
+
+public class FeaturedPhoto {
+    [Column(IsIdentity = true, IsPrimary = true)]
+    public int Id { get; set; }
+
+    public string PhotoId { get; set; }
+    public Photo Photo { get; set; }
+}
