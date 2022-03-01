8b7e8a34ddba3b64a2729b0d0f7edb470ae58aca
DealiAxy <dealiaxy@gmail.com>
2022-03-01 15:01:03 +0800

    增加设置推荐图片和取消的接口

--- StarBlog.Web/Apis/PhotoController.cs
+++ StarBlog.Web/Apis/PhotoController.cs
@@ -33,8 +33,9 @@ public class PhotoController : ControllerBase {
     [HttpGet("{id}")]
     public ApiResponse<Photo> Get(string id) {
         var photo = _photoService.GetById(id);
-        if (photo == null) return ApiResponse.NotFound();
-        return new ApiResponse<Photo> { Data = photo };
+        return photo == null 
+            ? ApiResponse.NotFound($"图片 {id} 不存在") 
+            : new ApiResponse<Photo> { Data = photo };
     }
 
     [Authorize]
@@ -51,11 +52,37 @@ public class PhotoController : ControllerBase {
     [HttpDelete("{id}")]
     public ApiResponse Delete(string id) {
         var photo = _photoService.GetById(id);
-        if (photo == null) return ApiResponse.NotFound();
+        if (photo == null) return ApiResponse.NotFound($"图片 {id} 不存在");
         var rows = _photoService.DeleteById(id);
         return rows > 0
             ? ApiResponse.Ok($"deleted {rows} rows.")
             : ApiResponse.Error(Response, "deleting failed.");
+    }
+
+    /// <summary>
+    /// 设置为推荐图片
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpPost("{id}/[action]")]
+    public ApiResponse<FeaturedPhoto> SetFeatured(string id) {
+        var photo = _photoService.GetById(id);
+        return photo == null
+            ? ApiResponse.NotFound($"图片 {id} 不存在")
+            : new ApiResponse<FeaturedPhoto>(_photoService.AddFeaturedPhoto(photo));
+    }
+
+    /// <summary>
+    /// 取消推荐
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpPost("{id}/[action]")]
+    public ApiResponse CancelFeatured(string id) {
+        var photo = _photoService.GetById(id);
+        if (photo == null) return ApiResponse.NotFound($"图片 {id} 不存在");
+        var rows = _photoService.DeleteFeaturedPhoto(photo);
+        return ApiResponse.Ok($"deleted {rows} rows.");
     }
 
     /// <summary>
