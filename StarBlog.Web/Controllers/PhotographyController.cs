365bf09e3cc7b638dc89c98db9167a831d79b108
DealiAxy <dealiaxy@gmail.com>
2022-03-02 22:35:12 +0800

    增加随机图片功能

--- StarBlog.Web/Controllers/PhotographyController.cs
+++ StarBlog.Web/Controllers/PhotographyController.cs
@@ -1,5 +1,6 @@
 ﻿using FreeSql;
 using Microsoft.AspNetCore.Mvc;
+using StarBlog.Contrib.SiteMessage;
 using StarBlog.Data.Models;
 using StarBlog.Web.Services;
 using StarBlog.Web.ViewModels;
@@ -8,14 +9,27 @@ namespace StarBlog.Web.Controllers;
 
 public class PhotographyController : Controller {
     private readonly PhotoService _photoService;
+    private readonly Messages _messages;
 
-    public PhotographyController(PhotoService photoService) {
+    public PhotographyController(PhotoService photoService, Messages messages) {
         _photoService = photoService;
+        _messages = messages;
     }
 
     public IActionResult Index(int page = 1, int pageSize = 10) {
         return View(new PhotographyViewModel {
             Photos = _photoService.GetPagedList(page, pageSize)
         });
+    }
+
+    public IActionResult Photo(string id) {
+        return View(_photoService.GetById(id));
+    }
+
+    public IActionResult RandomPhoto() {
+        var items = _photoService.GetAll();
+        var rndItem = items[new Random().Next(items.Count)];
+        _messages.Info($"随机推荐了图片 <b>{rndItem.Title}</b> 给你~");
+        return RedirectToAction(nameof(Photo), new {id = rndItem.Id});
     }
 }
