5789c82054f076e9649c7e18ad0dc6083c6c9e53
DealiAxy <dealiaxy@gmail.com>
2022-03-07 19:46:30 +0800

    分类加了是否可见字段

--- StarBlog.Data/Models/Category.cs
+++ StarBlog.Data/Models/Category.cs
@@ -7,9 +7,14 @@ public class Category {
     public int Id { get; set; }
 
     public string Name { get; set; }
-    
+
     public int ParentId { get; set; }
     public Category? Parent { get; set; }
+
+    /// <summary>
+    /// 分类是否可见
+    /// </summary>
+    public bool Visible { get; set; } = true;
 
     public List<Post> Posts { get; set; }
 }
