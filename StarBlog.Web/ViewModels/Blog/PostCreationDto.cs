3ed98e7c50571f52ff58db253f5689b061b184b9
DealiAxy <deali@live.com>
2022-03-23 00:21:35 +0800

    增加了新增博客的dto

---
+++ StarBlog.Web/ViewModels/Blog/PostCreationDto.cs
@@ -0,0 +1,23 @@
+namespace StarBlog.Web.ViewModels.Blog; 
+
+public class PostCreationDto {
+    /// <summary>
+    /// 标题
+    /// </summary>
+    public string? Title { get; set; }
+
+    /// <summary>
+    /// 梗概
+    /// </summary>
+    public string? Summary { get; set; }
+
+    /// <summary>
+    /// 内容（markdown格式）
+    /// </summary>
+    public string? Content { get; set; }
+    
+    /// <summary>
+    /// 分类ID
+    /// </summary>
+    public int CategoryId { get; set; }
+}
