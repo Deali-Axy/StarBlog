67dec74fde61aa8972e0278a6ecef4d0463ec603
DealiAxy <dealiaxy@gmail.com>
2022-03-25 16:46:35 +0800

    正则表达式测试完成

---
+++ demo/RegTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+var title = "（未修改）在-NET-Core中使用MongoDB明细教程(1)-驱动基础及文档插入";
+
+var result= Regex.Match(title,@"（(.+)）(.+)");
+
+foreach (var item in result.Groups) {
+    Console.WriteLine(item);
+}
