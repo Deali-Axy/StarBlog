81093a80765eda5d7f0421727baca157ba530668
DealiAxy <dealiaxy@gmail.com>
2022-06-11 11:53:44 +0800

    新增仪表盘接口

---
+++ StarBlog.Web/Apis/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Contrib.CLRStats;
+using StarBlog.Web.ViewModels.Response;
+
+namespace StarBlog.Web.Apis;
+
+[Authorize]
+[ApiController]
+[Route("Api/[controller]")]
+[ApiExplorerSettings(GroupName = "common")]
+public class DashboardController : ControllerBase {
+    [HttpGet("[action]")]
+    public ApiResponse ClrStats() {
+        return ApiResponse.Ok(CLRStatsUtils.GetCurrentClrStats());
+    }
+}
