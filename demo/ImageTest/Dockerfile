9ca16b2a5ea1d855e0354a8f7804d523efcc797d
DealiAxy <dealiaxy@gmail.com>
2022-06-16 17:53:40 +0800

    暂时完成图片 resize + crop 功能

---
+++ demo/ImageTest/Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["demo/ImageTest/ImageTest.csproj", "ImageTest/"]
+RUN dotnet restore "demo/ImageTest/ImageTest.csproj"
+COPY . .
+WORKDIR "/src/ImageTest"
+RUN dotnet build "ImageTest.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ImageTest.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ImageTest.dll"]
---
+++ demo/ImageTest/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+Console.WriteLine("Hello, World!");
+
+var imgPath1 = @"D:\Code\StarBlog\StarBlog.Web\wwwroot\assets\photography\10.jpg";
+var imgPath2 = @"D:\Code\StarBlog\StarBlog.Web\wwwroot\assets\photography\89048309f52ff0d70f51ac3c38a39c80.jpg";
+
+
+void GetImage(string imagePath, int width, int height) {
+    using var image = Image.Load(imagePath);
+    int newWidth;
+    int newHeight;
+    Rectangle cropRect;
+
+    if (image.Width > image.Height) {
+        image.Mutate(a => a.Resize(0, height));
+        cropRect = new Rectangle((image.Width - width) / 2, 0, width, height);
+    }
+    else {
+        image.Mutate(a => a.Resize(width, 0));
+        cropRect = new Rectangle(0, (image.Height - height) / 2, width, height);
+    }
+
+    image.Mutate(a => a.Crop(cropRect));
+    image.SaveAsPng(@"D:\Code\StarBlog\demo\ImageTest\images\output.png");
+}
+
+GetImage(imgPath2, 200, 200);
