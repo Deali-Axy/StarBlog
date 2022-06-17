27884d6f5f0d93271a20608fac23679dc50ac08e
DealiAxy <dealiaxy@gmail.com>
2022-06-17 16:41:23 +0800

    图片尺寸调整demo

--- demo/ImageTest/Program.cs
+++ demo/ImageTest/Program.cs
@@ -5,27 +5,45 @@ using SixLabors.ImageSharp.Processing;
 
 Console.WriteLine("Hello, World!");
 
-var imageList = new List<string> {
-    "1xdqasb8hblhoo57xthdxu1v6.jpg", "2cs5jqz3o4orwvpr5i5oroyck.jpg", "2hngz6wxz7vzd8ncr6ym1rt7p.jpg", "2kjvh5zl9y0g9ovrqmkodaurt.jpg",
-    "wallhaven-753155.jpg",
-};
+var imageList = new List<string> { "70.jpg" };
 
 void GetImage(string imagePath, int width, int height) {
     using var image = Image.Load(imagePath);
     Rectangle cropRect;
-
+    int newWidth;
+    int newHeight;
+    
+    // 横屏图片
     if (image.Width > image.Height) {
-        image.Mutate(a => a.Resize(0, height));
-        cropRect = new Rectangle((image.Width - width) / 2, 0, width, height);
+        if (width > image.Width) {
+            newWidth = width;
+            newHeight = height;
+        }
+        else {
+            newHeight = height;
+            newWidth = image.Width / image.Height * newHeight;
+        }
+
+        cropRect = new Rectangle((newWidth - width) / 2, 0, width, height);
     }
+    // 竖屏图片
     else {
-        image.Mutate(a => a.Resize(width, 0));
-        cropRect = new Rectangle(0, (image.Height - height) / 2, width, height);
+        if (height > image.Height) {
+            newWidth = width;
+            newHeight = height;
+        }
+        else {
+            newWidth = width;
+            newHeight = newWidth * image.Height / image.Width;
+        }
+
+        cropRect = new Rectangle(0, (newHeight - height) / 2, width, height);
     }
 
+    image.Mutate(a => a.Resize(newWidth, newHeight));
     image.Mutate(a => a.Crop(cropRect));
     image.SaveAsPng(@"images\output\output.png");
 }
 
-var inputImage = Path.Combine("images", imageList[3]);
-GetImage(inputImage, 200, 200);
+var inputImage = Path.Combine("images", imageList[0]);
+GetImage(inputImage, 600, 400);
