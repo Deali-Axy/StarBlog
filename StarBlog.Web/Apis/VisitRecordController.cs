c82aea5a77f90f64d44d4566d29eeba092bbe8cc
DealiAxy <dealiaxy@gmail.com>
2022-06-22 22:27:46 +0800

    新增查看统计记录的接口

---
+++ StarBlog.Web/Apis/VisitRecordController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Data.Models;
+using StarBlog.Web.Services;
+using StarBlog.Web.ViewModels.Response;
+
+namespace StarBlog.Web.Apis;
+
+[Authorize]
+[ApiController]
+[Route("Api/[controller]")]
+[ApiExplorerSettings(GroupName = "admin")]
+public class VisitRecordController : ControllerBase {
+    private readonly VisitRecordService _service;
+
+    public VisitRecordController(VisitRecordService service) {
+        _service = service;
+    }
+
+    [HttpGet("All")]
+    public ApiResponse<List<VisitRecord>> GetAll() {
+        return new ApiResponse<List<VisitRecord>>(_service.GetAll());
+    }
+
+    [HttpGet("{id:int}")]
+    public ApiResponse<VisitRecord> GetById(int id) {
+        var item = _service.GetById(id);
+        return item == null ? ApiResponse.NotFound() : new ApiResponse<VisitRecord>(item);
+    }
+}
---
+++ StarBlog.Web/Services/VisitRecordService.cs
@@ -0,0 +1,21 @@
+﻿using FreeSql;
+using StarBlog.Data.Models;
+
+namespace StarBlog.Web.Services;
+
+public class VisitRecordService {
+    private readonly IBaseRepository<VisitRecord> _repo;
+
+    public VisitRecordService(IBaseRepository<VisitRecord> repo) {
+        _repo = repo;
+    }
+
+    public VisitRecord? GetById(int id) {
+        var item = _repo.Where(a => a.Id == id).First();
+        return item;
+    }
+
+    public List<VisitRecord> GetAll() {
+        return _repo.Select.ToList();
+    }
+}
