7629f03f1b3edc3e4221285c5bd20678f52883b6
DealiAxy <dealiaxy@gmail.com>
2022-06-27 00:04:35 +0800

    完成了生成指定尺寸图片的新算法

--- demo/ImageTest/Program.cs
+++ demo/ImageTest/Program.cs
@@ -1,18 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Processing;
 
 Console.WriteLine("Hello, World!");
 
-var imageList = new List<string> { "70.jpg" };
+List<string> ScanImages() {
+    var data = new List<string>();
+    foreach (var file in Directory.GetFiles("images")) {
+        Console.WriteLine(file);
+        data.Add(file);
+    }
+
+    return data;
+}
+
+// 求最大公约数和最小公倍数
+(int, int) Test(int num1, int num2) {
+    //定义变量保存两数乘积
+    var product = num1 * num2;
+
+    //定义变量临时保存除余结果
+    var temp = num1 % num2;
+    // 辗转相除法
+    do {
+        num1 = num2;
+        num2 = temp;
+        temp = num1 % num2;
+    } while (temp != 0);
+
+    // 最大公约数, 最小公倍数
+    return (num2, product / num2);
+}
 
 void GetImage(string imagePath, int width, int height) {
-    using var image = Image.Load(imagePath);
+    var image = Image.Load(imagePath);
+
     Rectangle cropRect;
     int newWidth;
     int newHeight;
-    
+
     // 横屏图片
     if (image.Width > image.Height) {
         if (width > image.Width) {
@@ -42,8 +70,54 @@ void GetImage(string imagePath, int width, int height) {
 
     image.Mutate(a => a.Resize(newWidth, newHeight));
     image.Mutate(a => a.Crop(cropRect));
-    image.SaveAsPng(@"images\output\output.png");
+    image.SaveAsJpeg(@"images\output\output.jpg");
 }
 
-var inputImage = Path.Combine("images", imageList[0]);
-GetImage(inputImage, 600, 400);
+async Task<(Image, IImageFormat)> GetImage2(string imagePath, int width, int height) {
+    await using var fileStream = new FileStream(imagePath, FileMode.Open);
+    var (image, format) = await Image.LoadWithFormatAsync(fileStream);
+
+    Console.WriteLine($"origin image={image.Width},{image.Height}");
+
+    // 输出尺寸超出原图片尺寸，放大
+    if (width > image.Width && height > image.Height) {
+        image.Mutate(a => a.Resize(width, height));
+    }
+    else if (width > image.Width || height > image.Height) {
+        // 改变比例大的边
+        if (width / image.Width < height / image.Height)
+            image.Mutate(a => a.Resize(0, height));
+        else
+            image.Mutate(a => a.Resize(width, 0));
+    }
+
+    Console.WriteLine($"Resize={image.Width},{image.Height}");
+
+    // 将输入的尺寸作为裁剪比例
+    var (temp, _) = Test(width, height);
+    var (scaleWidth, scaleHeight) = (width / temp, height / temp);
+    var cropWidth = image.Width;
+    var cropHeight = image.Width / scaleWidth * scaleHeight;
+    if (cropHeight > image.Height) {
+        cropHeight = image.Height;
+        cropWidth = image.Height / scaleHeight * scaleWidth;
+    }
+
+    var cropRect = new Rectangle((image.Width - cropWidth) / 2, (image.Height - cropHeight) / 2, cropWidth, cropHeight);
+    Console.WriteLine(cropRect.ToString());
+    image.Mutate(a => a.Crop(cropRect));
+    image.Mutate(a => a.Resize(width, height));
+
+    return (image, format);
+}
+
+async void Run() {
+    var (image, _) = await GetImage2(ScanImages()[0], 200, 250);
+    await image.SaveAsJpegAsync(@"images\output\output.jpg");
+    Console.WriteLine("saved.");
+}
+
+Run();
+
+
+Console.Read();
