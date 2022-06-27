c70764602e369297158937c83e5cde084af54b48
DealiAxy <dealiaxy@gmail.com>
2022-06-27 21:24:32 +0800

    add dto

---
+++ StarBlog.Web/ViewModels/Links/LinkCreationDto.cs
@@ -0,0 +1,23 @@
+﻿namespace StarBlog.Web.ViewModels.Links; 
+
+public class LinkCreationDto {
+    /// <summary>
+    /// 网站名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 介绍
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// 网址
+    /// </summary>
+    public string Url { get; set; }
+
+    /// <summary>
+    /// 是否显示
+    /// </summary>
+    public bool Visible { get; set; }
+}
