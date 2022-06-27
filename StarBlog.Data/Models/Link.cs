fa61c17f694db4280cb18725e699d6e91ca2bf5c
DealiAxy <dealiaxy@gmail.com>
2022-06-27 16:13:21 +0800

    友情链接模块建模

---
+++ StarBlog.Data/Models/Link.cs
@@ -0,0 +1,26 @@
+﻿namespace StarBlog.Data.Models;
+
+/// <summary>
+/// 友情链接
+/// </summary>
+public class Link {
+    /// <summary>
+    /// 网站名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 介绍
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// 网址
+    /// </summary>
+    public string Url { get; set; }
+
+    /// <summary>
+    /// 是否显示
+    /// </summary>
+    public bool Visible { get; set; }
+}
---
+++ StarBlog.Data/Models/LinkExchange.cs
@@ -0,0 +1,42 @@
+﻿namespace StarBlog.Data.Models;
+
+/// <summary>
+/// 友情链接申请记录
+/// </summary>
+public class LinkExchange {
+    /// <summary>
+    /// 网站名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 介绍
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// 网址
+    /// </summary>
+    public string Url { get; set; }
+
+    /// <summary>
+    /// 站长
+    /// </summary>
+    public string WebMaster { get; set; }
+
+    /// <summary>
+    /// 联系邮箱
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// 是否已验证
+    /// <para>友情链接需要验证后才显示在网站上</para>
+    /// </summary>
+    public bool Verified { get; set; }
+
+    /// <summary>
+    /// 申请时间
+    /// </summary>
+    public DateTime ApplyTime { get; set; }
+}
