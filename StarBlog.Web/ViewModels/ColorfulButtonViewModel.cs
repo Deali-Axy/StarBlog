0697620b2b24426e2500d66632c00c128b5d4f52
DealiAxy <dealiaxy@gmail.com>
2022-06-28 10:30:58 +0800

    封装了随机颜色的按钮组件

---
+++ StarBlog.Web/ViewModels/ColorfulButtonViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StarBlog.Web.ViewModels;
+
+public static class LinkTarget {
+    public const string Blank = "_blank";
+    public const string Parent = "_parent";
+    public const string Self = "_self";
+    public const string Top = "_top";
+}
+
+public class ColorfulButtonViewModel {
+    public string Name { get; set; }
+    public string Url { get; set; } = "#";
+    public string Target { get; set; } = "_self";
+}
