df67ec5e6b80c2be5eccf6e7b7ed7aba10350f16
DealiAxy <dealiaxy@gmail.com>
2022-07-11 22:10:22 +0800

    新增配置项模型

---
+++ StarBlog.Data/Models/ConfigItem.cs
@@ -0,0 +1,12 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace StarBlog.Data.Models;
+
+public class ConfigItem {
+    [Column(IsIdentity = true, IsPrimary = true)]
+    public int Id { get; set; }
+
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public string? Description { get; set; }
+}
