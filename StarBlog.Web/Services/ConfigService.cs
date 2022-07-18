bbc2f39ddf91ad21b5a519d60c2cc78efd2e4071
DealiAxy <dealiaxy@gmail.com>
2022-07-18 15:43:32 +0800

    把配置接口搞定了！

--- StarBlog.Web/Services/ConfigService.cs
+++ StarBlog.Web/Services/ConfigService.cs
@@ -26,6 +26,15 @@ public class ConfigService {
         return _repo.InsertOrUpdate(item);
     }
 
+    public int? Update(string key, string value, string? description = default) {
+        var item = GetByKey(key);
+        if (item == null) return null;
+
+        item.Value = value;
+        if (description != null) item.Description = description;
+        return _repo.Update(item);
+    }
+
     public int DeleteById(int id) {
         return _repo.Delete(a => a.Id == id);
     }
@@ -40,7 +49,7 @@ public class ConfigService {
             return item == null ? "" : item.Value;
         }
         set {
-            var item = GetByKey(key) ?? new ConfigItem {Key = key, Value = value};
+            var item = GetByKey(key) ?? new ConfigItem { Key = key, Value = value };
             AddOrUpdate(item);
         }
     }
---
+++ StarBlog.Web/ViewModels/Config/ConfigItemCreationDto.cs
@@ -0,0 +1,5 @@
+﻿public class ConfigItemCreationDto {
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public string? Description { get; set; }
+}
