3d6226d036d61fc31b00cc9df9a32912fba88d9a
DealiAxy <dealiaxy@gmail.com>
2022-07-20 17:29:32 +0800

    把初始化功能初步搞定了~！

--- StarBlog.Web/Controllers/HomeController.cs
+++ StarBlog.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
+using StarBlog.Contrib.SiteMessage;
+using StarBlog.Data.Models;
 using StarBlog.Web.Services;
 using StarBlog.Web.ViewModels;
 
@@ -9,12 +12,14 @@ public class HomeController : Controller {
     private readonly PhotoService _photoService;
     private readonly CategoryService _categoryService;
     private readonly LinkService _linkService;
+    private readonly Messages _messages;
 
-    public HomeController(BlogService blogService, PhotoService photoService, CategoryService categoryService, LinkService linkService) {
+    public HomeController(BlogService blogService, PhotoService photoService, CategoryService categoryService, LinkService linkService, Messages messages) {
         _blogService = blogService;
         _photoService = photoService;
         _categoryService = categoryService;
         _linkService = linkService;
+        _messages = messages;
     }
 
     public IActionResult Index() {
@@ -26,5 +31,37 @@ public class HomeController : Controller {
             FeaturedCategories = _categoryService.GetFeaturedCategories(),
             Links = _linkService.GetAll()
         });
+    }
+
+    [HttpGet]
+    public IActionResult Init([FromServices] ConfigService conf) {
+        if (conf["is_init"] == "true") {
+            _messages.Error("已经完成初始化！");
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(new InitViewModel {
+            Host = conf["host"]
+        });
+    }
+
+    [HttpPost]
+    public IActionResult Init([FromServices] ConfigService conf, [FromServices] IBaseRepository<User> userRepo, InitViewModel vm) {
+        if (!ModelState.IsValid) return View();
+
+        // 保存配置
+        conf["host"] = vm.Host;
+        conf["is_init"] = "true";
+
+        // 创建用户
+        // todo 这里暂时存储明文密码，后期要换成MD5加密存储
+        userRepo.Insert(new User {
+            Id = Guid.NewGuid().ToString(),
+            Name = vm.Username,
+            Password = vm.Password
+        });
+
+        _messages.Success("初始化完成！");
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ StarBlog.Web/ViewModels/InitViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StarBlog.Web.ViewModels; 
+
+public class InitViewModel {
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Host { get; set; }
+}
