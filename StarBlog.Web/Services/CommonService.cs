1a8b710021b6d8c94fc5edadc1be615b798814f5
DealiAxy <dealiaxy@gmail.com>
2022-07-27 15:32:24 +0800

    新增一个公共服务，目前只有下载文件一个功能，使用IHttpClientFactory

---
+++ StarBlog.Web/Services/CommonService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using StarBlog.Contrib.Utils;
+
+namespace StarBlog.Web.Services;
+
+/// <summary>
+/// 一些公共服务
+/// </summary>
+public class CommonService {
+    private readonly ILogger<CommonService> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CommonService(ILogger<CommonService> logger, IHttpClientFactory httpClientFactory) {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="savePath">保存路径，需要完整路径</param>
+    /// <returns></returns>
+    public async Task<string?> DownloadFile(string url, string savePath) {
+        var httpClient = _httpClientFactory.CreateClient();
+        try {
+            var resp = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+
+            var fileName = GuidUtils.GuidTo16String() + Path.GetExtension(url);
+            var filePath = Path.Combine(savePath, WebUtility.UrlEncode(fileName));
+            await using var fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            await resp.Content.CopyToAsync(fs);
+
+            return fileName;
+        }
+        catch (Exception ex) {
+            _logger.LogError("下载文件出错，信息：{Error}", ex);
+            return null;
+        }
+    }
+}
