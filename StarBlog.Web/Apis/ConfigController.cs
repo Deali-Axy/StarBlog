6a755aeacb54e3a41016f9809da061030f2551ee
DealiAxy <dealiaxy@gmail.com>
2022-10-17 22:44:15 +0800

    配置中心接口添加key唯一性判断

--- StarBlog.Web/Apis/ConfigController.cs
+++ StarBlog.Web/Apis/ConfigController.cs
@@ -30,12 +30,18 @@ public class ConfigController : ControllerBase {
     }
 
     [HttpPost]
-    public ConfigItem Add(ConfigItemCreationDto dto) {
-        return _service.AddOrUpdate(new ConfigItem {
+    public ApiResponse<ConfigItem> Add(ConfigItemCreationDto dto) {
+        var item = _service.GetByKey(dto.Key);
+        if (item != null) {
+            return ApiResponse.BadRequest($"key {dto.Key} 已存在！");
+        }
+
+        item = _service.AddOrUpdate(new ConfigItem {
             Key = dto.Key,
             Value = dto.Value,
             Description = dto.Description
         });
+        return new ApiResponse<ConfigItem>(item);
     }
 
     [HttpPut("{key}")]
@@ -50,7 +56,6 @@ public class ConfigController : ControllerBase {
     [HttpDelete("{key}")]
     public ApiResponse Delete(string key) {
         var item = _service.GetByKey(key);
-        if (item == null) return ApiResponse.NotFound();
-        return ApiResponse.Ok($"已删除 {_service.DeleteByKey(key)} 条数据");
+        return item == null ? ApiResponse.NotFound() : ApiResponse.Ok($"已删除 {_service.DeleteByKey(key)} 条数据");
     }
 }
