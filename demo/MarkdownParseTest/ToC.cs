9cff15419c5285d1c505142e906c95be00231473
DealiAxy <dealiaxy@gmail.com>
2022-10-24 12:39:06 +0800

    视图搞个Markdown解析toc功能

---
+++ demo/MarkdownParseTest/ToC.cs
@@ -0,0 +1,26 @@
+﻿using Markdig;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
+namespace MarkdownParseTest;
+
+class Header {
+    public string Text { get; set; }
+    public int Level { get; set; }
+}
+
+public static class ToC {
+    public static void ExtractToc(string filepath) {
+        var md = File.ReadAllText(filepath);
+        var document = Markdown.Parse(md);
+        
+        foreach (var node in document.AsEnumerable()) {
+            if (node is ParagraphBlock {Inline: { }} paragraphBlock) {
+                foreach (var inline in paragraphBlock.Inline) {
+                    Console.WriteLine(inline);
+                }
+                Console.WriteLine(paragraphBlock.Inline);   
+            }
+        }
+    }
+}
