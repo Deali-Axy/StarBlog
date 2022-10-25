a6f75465356d3df101fdafbac0ab31a79b1611bd
DealiAxy <dealiaxy@gmail.com>
2022-10-25 11:30:06 +0800

    昨天写的，没搞定，然后昨天搞定了

--- demo/MarkdownParseTest/ToC.cs
+++ demo/MarkdownParseTest/ToC.cs
@@ -1,25 +1,30 @@
 ﻿using Markdig;
 using Markdig.Syntax;
-using Markdig.Syntax.Inlines;
 
 namespace MarkdownParseTest;
 
 class Header {
-    public string Text { get; set; }
+    public string? Text { get; set; }
     public int Level { get; set; }
+}
+
+class TocNode {
+    public string? Text { get; set; }
+    public string Href { get; set; }
+    public List<string> Tags { get; set; }
+    public List<TocNode> Nodes { get; set; }
 }
 
 public static class ToC {
     public static void ExtractToc(string filepath) {
         var md = File.ReadAllText(filepath);
         var document = Markdown.Parse(md);
-        
+        var headers = new List<Header>();
+
         foreach (var node in document.AsEnumerable()) {
-            if (node is ParagraphBlock {Inline: { }} paragraphBlock) {
-                foreach (var inline in paragraphBlock.Inline) {
-                    Console.WriteLine(inline);
-                }
-                Console.WriteLine(paragraphBlock.Inline);   
+            if (node is HeadingBlock block) {
+                Console.WriteLine("{0}: {1}", block.Level, block.Inline?.FirstChild);
+                headers.Add(new Header { Level = block.Level, Text = block.Inline?.FirstChild?.ToString() });
             }
         }
     }
