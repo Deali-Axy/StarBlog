7447ed423d7064580300aed898c3a39a09de4015
DealiAxy <dealiaxy@gmail.com>
2022-11-30 20:42:29 +0800

    重新让首页的一句诗和一言恢复工作~

---
+++ StarBlog.Web/Models/DataAcqResp.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StarBlog.Web.Models; 
+
+public class DataAcqResp<T> {
+    [JsonPropertyName("status")]
+    public string Status { get; set; }
+
+    [JsonPropertyName("msg")]
+    public string Msg { get; set; }
+
+    [JsonPropertyName("data")]
+    public T Data { get; set; }
+}
