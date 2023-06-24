c99fa6ce1c66cabb00c7983af98f0436a36b353e
DealiAxy <dealiaxy@gmail.com>
2023-06-24 21:28:23 +0800

    🎇添加RSS引导页面

--- StarBlog.Web/Controllers/RssController.cs
+++ StarBlog.Web/Controllers/RssController.cs
@@ -7,26 +7,33 @@ using StarBlog.Data.Models;
 using StarBlog.Web.Extensions;
 using StarBlog.Web.Services;
 
-namespace StarBlog.Web.Apis.Blog;
+namespace StarBlog.Web.Controllers;
 
-[ApiController]
-[Route("feed")]
 [ApiExplorerSettings(IgnoreApi = true)]
-public class RssController : ControllerBase {
+public class RssController : Controller {
     private readonly IBaseRepository<Post> _postRepo;
+    private readonly ConfigService _conf;
 
-    public RssController(IBaseRepository<Post> postRepo) {
+    public RssController(IBaseRepository<Post> postRepo, ConfigService conf) {
         _postRepo = postRepo;
+        _conf = conf;
+    }
+
+    [HttpGet]
+    public IActionResult Index() {
+        var feedUrl = $"{_conf["Host"]}/feed";
+        ViewBag.FeedUrl = feedUrl;
+        return View();
     }
 
     [ResponseCache(Duration = 1200)]
-    [HttpGet]
-    public async Task<IActionResult> Index() {
+    [HttpGet("feed")]
+    public async Task<IActionResult> Feed() {
         var posts = await _postRepo.Where(a => a.IsPublish && a.CreationTime.Year == DateTime.Now.Year)
             .OrderByDescending(a => a.LastUpdateTime)
             .Include(a => a.Category)
             .ToListAsync();
-        
+
         var feed = new SyndicationFeed(
             "StarBlog",
             "「程序设计实验室」 专注于互联网热门新技术探索与团队敏捷开发实践，包括架构设计、机器学习与数据分析算法、移动端开发、Linux、Web前后端开发等，欢迎一起探讨技术，分享学习实践经验。",
