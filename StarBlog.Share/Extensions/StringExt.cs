14f994be7ad9ec93aed47a834d85d9173d9aa3c6
DealiAxy <dealiaxy@gmail.com>
2023-12-01 16:24:55 +0800

    🆕全新的文章列表UI

--- StarBlog.Share/Extensions/StringExt.cs
+++ StarBlog.Share/Extensions/StringExt.cs
@@ -4,11 +4,14 @@ namespace StarBlog.Share.Extensions;
 
 public static class StringExt {
     public static string Limit(this string str, int length) {
-        if (str.Length <= length) {
-            return str;
-        }
+        return str.Length <= length ? str : str[..length];
+    }
 
-        return str[..length];
+    /// <summary>
+    /// 限制字符串显示长度并在末尾添加省略号
+    /// </summary>
+    public static string LimitWithEllipsis(this string str, int length) {
+        return str.Length <= length ? str : $"{str[..length]}...";
     }
 
     public static string ToSHA256(this string source) {
