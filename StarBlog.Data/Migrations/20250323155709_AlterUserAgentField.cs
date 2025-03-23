c0e6fea99071fadc7436809f86cba7ac4d3a32d9
DealiAxy <dealiaxy@gmail.com>
2025-03-24 00:02:49 +0800

    refactor: 将 UserAgent 字段改为可为空
    修改 VisitRecord 模型中的 UserAgent 字段，使其可为空，以更好地处理用户设备信息可能缺失的情况。同时更新了相关的数据库迁移文件。

---
+++ StarBlog.Data/Migrations/20250323155709_AlterUserAgentField.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarBlog.Data.Migrations
+{
+    public partial class AlterUserAgentField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_agent",
+                table: "visit_record",
+                type: "TEXT",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 1024);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_agent",
+                table: "visit_record",
+                type: "TEXT",
+                maxLength: 1024,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 1024,
+                oldNullable: true);
+        }
+    }
+}
