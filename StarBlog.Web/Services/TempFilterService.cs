daac58b078a51c85a0e1e74d8772ff38f6902e5a
DealiAxy <dealiaxy@gmail.com>
2025-04-27 23:03:35 +0800

    refactor(TempFilterService): 添加日志记录并优化文件读取逻辑
    引入 `ILogger` 记录日志，增加对 `words.json` 文件存在性的检查，避免文件不存在时抛出异常。若文件不存在，初始化空敏感词列表并记录警告日志。

--- StarBlog.Web/Services/TempFilterService.cs
+++ StarBlog.Web/Services/TempFilterService.cs
@@ -4,13 +4,25 @@ using CodeLab.Share.Contrib.StopWords;
 namespace StarBlog.Web.Services;
 
 public class TempFilterService {
+    private readonly ILogger<TempFilterService> _logger;
     private readonly StopWordsToolkit _toolkit;
 
     public StopWordsToolkit Toolkit => _toolkit;
 
-    public TempFilterService() {
-        var words = JsonSerializer.Deserialize<IEnumerable<Word>>(File.ReadAllText("words.json"));
-        _toolkit = new StopWordsToolkit(words!.Select(a => a.Value));
+    public TempFilterService(ILogger<TempFilterService> logger) {
+        _logger = logger;
+        IEnumerable<Word>? words = null;
+        const string wordsPath = "words.json";
+        if (File.Exists(wordsPath)) {
+            words = JsonSerializer.Deserialize<IEnumerable<Word>>(File.ReadAllText(wordsPath));
+        }
+        else {
+            _logger.LogWarning("未找到 words.json 文件，评论敏感词检测服务可能无法正常工作！");
+        }
+
+        _toolkit = words != null
+            ? new StopWordsToolkit(words.Select(a => a.Value))
+            : new StopWordsToolkit(Array.Empty<string>());
     }
 
     public bool CheckBadWord(string word) {
